4dea0b859e556e1043ab80a31e84bc5857da4369
PHÁT HUỲNH ĐỖ TẤN <1612488@student.hcmus.edu.vn>
2019-10-18 00:16:13 +0700

    Add Branch meokeumeo for BuffSystem

---
+++ Assets/System/BuffSystem/BuffControl.cs
@@ -0,0 +1,26 @@
+﻿using Assets.System.BuffSystem;
+using Crom.System.UnitSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffControl : MonoBehaviour
+{
+    public Unit unitGiveEffect { get; set; }
+    public Unit unitReceiveEffect { get; set; }
+    public BuffType bufftype { get; set; }
+    public Time timeCountDown { get; set; }
+    public float value { get; set; }
+
+    //Init
+    public BuffControl(Unit UnitGiveEffect, Unit UnitReceiveEffect)
+    {
+        
+    }
+
+    //
+    public void TimeOut()
+    {
+
+    }
+}
