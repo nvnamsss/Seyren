b4e3e28eb5b2b97778fe2823b80ab7df7e303211
nvnamsss <nvnam.c@gmail.com>
2020-02-13 10:15:28 +0700

    implementing QuestSystem

---
+++ System/QuestSystem/HierachyCondition.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base2D.System.QuestSystem
+{
+    public class HierarchyCondition
+    {
+        /* Its called hierarchy thus must have tree in there
+         * and of course its non-balance
+         * due to this tree is preferred performance in query (For real-time)
+         * thus Unlock must be O(1), its mean when a node in tree is modify, all related branch must update
+         * and of course data will be stored to ensure unlock take O(1).
+         * So time complexity will be O(1) and memory complexity will be O(n)
+         * Should it easily to balance depend on Size??? Seem no need
+         * Should I apply balance to this tree?
+         */
+        //ancestor
+        //descendant
+        /// <summary>
+        /// Demetermine if this condition is satisfied
+        /// </summary>
+        public bool Unlocked { get; }
+        public bool IsAncenstor(HierarchyCondition condition)
+        {
+            if (Ancestor.Count == 0)
+            {
+                return false;
+            }
+
+            if (Ancestor.Contains(condition))
+            {
+                return true;
+            }
+
+            for (int loop = 0; loop < Ancestor.Count; loop++)
+            {
+                if (Ancestor[loop].IsAncenstor(condition))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        protected List<HierarchyCondition> Ancestor;
+        /// <summary>
+        /// mark added condition as descendant of current condition
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        public bool AddCondition(HierarchyCondition condition)
+        {
+            if (IsAncenstor(condition))
+            {
+                return false;
+            }
+
+            Ancestor.Add(condition);
+            return true;
+        }
+
+        public bool Unlock()
+        {
+            return true;
+        }
+    }
+}
---
+++ System/QuestSystem/Quest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base2D.System.QuestSystem
+{
+    /// <summary>
+    /// Represents a quest in game
+    /// </summary>
+    public class Quest
+    {
+        /// <summary>
+        /// Quest's name
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// Quest's content
+        /// </summary>
+        public string Content;
+        /// <summary>
+        /// Quest's reward
+        /// </summary>
+        public IQuestReward Reward;
+    }
+}
