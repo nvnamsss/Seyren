c3aa1a13048d5bd4e5136085a9f0286be93e30bd
nvnamsss <nvnam.c@gmail.com>
2026-05-31 20:40:11 +0700

    feat: add AbilityContext for ability cast data

---
+++ com.voidstudio.seyren/Runtime/System/Abilities/AbilityContext.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Seyren.System.Units;
+using System.Collections.Generic;
+using Seyren.Visual;
+
+namespace Seyren.System.Abilities
+{
+    public class AbilityContext
+    {
+        public int level;
+        public IUnit caster;
+        public IUnit target;
+        public Vector3? location;
+        // vector start and end for ability that has a line effect
+        public Vector3? start;
+        public Vector3? end;
+        public Dictionary<string, IVisualEffect> visualEffects = new Dictionary<string, IVisualEffect>();
+    }
+
+}
